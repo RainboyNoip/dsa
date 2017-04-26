d00afef8ba639759c3dd46a4c671505f889485f1
Rainboy <597872644@qq.com>
2017-04-26 21:27:30 +0800

    [add] 目录 字体图标

--- src/category.vue
+++ src/category.vue
@@ -9,12 +9,13 @@
           <template v-for="category in categories">
             <li>
               <a class="folder">
+              <i :class="'iconfont '+pick_fruit(category.name)"></i>
               <span>{{category.name}}</span>
               </a>
               <ul class="nav nav-list">
                 <li v-for="list in category.list">
                   <a :href = "'#/'+list.href" target="_blank">
-                    <i :class="'iconfont '+(list.icon|| 'icon-default')"></i>
+                    <i :class="'iconfont '+pick_fruit(list.name)"></i>
                     {{list.name}}
                   </a>
                 </li>
@@ -38,9 +39,37 @@
       let self = this;
       let json_url = process.env.api+'dsa/categories.json';
       console.log("加载目录数据!");
-      $.getJSON(json_url,function(data){
-        self.categories = data.data
-      })
+      console.log(json_url);
+      $.getJSON(json_url)
+        .then(function(data){
+          //console.log(data.data)
+          self.categories = data.data;
+        })
+        .fail(function(){
+          console.log('加载'+json_url+'失败','请检查JSON格式');
+        })
+    },
+    methods:{
+      pick_fruit:function(str){
+        let self = this;
+        let _fonts = ['icon-boluo', 'icon-baixiangguo', 'icon-chengzi', 'icon-boluomei', 'icon-caomei', 'icon-dayouzi', 'icon-chelizi', 'icon-fanqie', 'icon-hamigua', 'icon-ganlan', 'icon-juzi', 'icon-heimei', 'icon-huolongguo', 'icon-hongmei', 'icon-lizi', 'icon-lanmei', 'icon-mangguo', 'icon-mihoutao', 'icon-longyan', 'icon-mugua', 'icon-lizi1', 'icon-ningmeng', 'icon-niuyouguo', 'icon-manyuemei', 'icon-pingguo', 'icon-qingning', 'icon-pipa', 'icon-shanzhu', 'icon-putao', 'icon-shiliu', 'icon-sangshen', 'icon-shizi', 'icon-shuimitao', 'icon-yezi', 'icon-xigua', 'icon-yangtao', 'icon-yingtao', 'icon-xiangjiao', 'icon-zao', 'icon-yangmei'];
+        return _fonts[ self.hashCode(str) %(_fonts.length)]
+      },
+      hashCode: function hash(input){
+          var hash = 5381;
+          var i = input.length - 1;
+
+          if(typeof input == 'string'){
+            for (; i > -1; i--)
+              hash += (hash << 5) + input.charCodeAt(i);
+          }
+          else{
+            for (; i > -1; i--)
+              hash += (hash << 5) + input[i];
+          }
+          var value = hash & 0x7FFFFFFF;
+          return value;
+      },
     }
   }
 </script>
